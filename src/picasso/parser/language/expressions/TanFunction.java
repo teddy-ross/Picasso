c84e2fcb1a70c7b2b9f59b8eb9c0d7dcdfbecca4
Sylvia Agatako <sagatako@mail.wlu.edu>
2024-11-19 16:30:54 -0500

    TanFunction

--- src/picasso/parser/language/expressions/TanFunction.java
+++ src/picasso/parser/language/expressions/TanFunction.java
@@ -3,16 +3,26 @@
  */
 package picasso.parser.language.expressions;
 
+import picasso.parser.language.ExpressionTreeNode;
+
+import picasso.parser.language.expressions.UnaryFunction;
+
 /**
  * 
  */
-public class TanFunction {
+public class TanFunction extends UnaryFunction{
+	
+	private final SineFunction sin;
+	private final CosineFunction cos; 
 
 	/**
 	 * 
 	 */
-	public TanFunction() {
-		// TODO Auto-generated constructor stub
+	public TanFunction(ExpressionTreeNode param) {
+		super(param);
+		
+		this.sin = new SinFunction(param);
+		this.cos = new CosFunction(param);
 	}
 
 }
