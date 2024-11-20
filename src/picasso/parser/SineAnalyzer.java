8f27164641e0b2a6d1738a3d8a7369586794bac5
sarina cusumano <cusumanos25@mail.wlu.edu>
2024-11-20 15:56:39 -0500

    sine analyzer

--- src/picasso/parser/SineAnalyzer.java
+++ src/picasso/parser/SineAnalyzer.java
@@ -1,9 +1,28 @@
 package picasso.parser;
 
-public class SineAnalyzer {
+import java.util.Stack;
 
-	public SineAnalyzer() {
-		// TODO Auto-generated constructor stub
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Sine;
+import picasso.parser.tokens.Token;
+
+/**
+ * Handles parsing the floor function.
+ * 
+ * @author Sarina Cusumano
+ * 
+ */
+public class SineAnalyzer extends UnaryFunctionAnalyzer {
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop(); // Need to remove the sine token
+		// the parameter is the next token(s) on the stack.
+		// But, it needs to be processed
+		ExpressionTreeNode paramETN = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+		return new Sine(paramETN);
 	}
 
 }
+
