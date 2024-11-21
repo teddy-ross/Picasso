73645aad1b538d4cfe034c9d0313d3e4719b4b71
Sanjog Basnet <sbasnet@mail.wlu.edu>
2024-11-20 21:51:54 -0500

    Added the classes for Division Functionality
    name in operations.prop
    BinaryOperator abstract class
    Division Expression
    DivisionToken
    DivideAnalyzer

---
+++ src/picasso/parser/DivideAnalyzer.java
@@ -0,0 +1,26 @@
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Division;
+import picasso.parser.tokens.Token;
+
+/**
+ * Handles parsing the slash/ divide or "division operation".
+ * 
+ * @author Sanjog Basnet
+ * 
+ */
+
+public class DivideAnalyzer implements SemanticAnalyzerInterface {
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop(); // Remove the slash token
+		ExpressionTreeNode leftETN = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
+		ExpressionTreeNode rightETN = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
+		return new Division(leftETN, rightETN);
+
+	}
+}
---
+++ src/picasso/parser/language/expressions/BinaryOperator.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package picasso.parser.language.expressions;
+import picasso.parser.language.ExpressionTreeNode;
+
+/**
+ * Represents the parent class for binary operators
+ * 
+ * @author Sanjog Basnet
+ */
+public abstract class BinaryOperator extends ExpressionTreeNode {
+
+	ExpressionTreeNode left;
+	ExpressionTreeNode right;
+	/**
+	 * 
+	 */
+	public BinaryOperator(ExpressionTreeNode left, ExpressionTreeNode right) {
+		this.left = left;
+		this.right = right;
+	}
+	/**
+	 * Returns the string representation in the format "x/y"
+	 * 
+	 */
+	
+	@Override
+	public String toString() {
+		return this.left + this.getOperator() + this.right;
+	}
+
+	/**
+	 * @param x
+	 * @param y
+	 * @return
+	 * @see picasso.parser.language.ExpressionTreeNode#evaluate(double, double)
+	 */
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o == this) {
+			return true;
+		}
+
+		if (!(o instanceof BinaryOperator)) {
+			return false;
+		}
+
+		// Make sure the objects are the same type
+
+		if (o.getClass() != this.getClass()) {
+			return false;
+		}
+
+		BinaryOperator bo = (BinaryOperator) o;
+
+		// check if their parameters are equal
+		if (!(this.left.equals(bo.left)) && (this.right.equals(bo.right))) {
+			return false;
+		}
+		return true;
+	}
+	
+	public abstract String getOperator(); 
+}
+
+
+
