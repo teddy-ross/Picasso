acec014e806d27edff877d2bb4466264fa22fb43
Teddy Ross <tross@mail.wlu.edu>
2024-11-22 11:15:58 -0500

    fixed version

--- src/tests/CeilTests/ExpressionTreeGeneratorTests.java
+++ src/tests/CeilTests/ExpressionTreeGeneratorTests.java
@@ -3,6 +3,20 @@
  * @author Nisha Walvekar
  * 
  */
+package tests.CeilTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import picasso.parser.ExpressionTreeGenerator;
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Addition;
+import picasso.parser.language.expressions.Ceil;
+import picasso.parser.language.expressions.X;
+import picasso.parser.language.expressions.Y;
+
+
 class ExpressionTreeGeneratorTests {
 
     private ExpressionTreeGenerator parser;
@@ -23,11 +37,11 @@ class ExpressionTreeGeneratorTests {
     @Test
     public void ceilFunctionTests() {
         // Test for simple ceiling of x
-        ExpressionTreeNode e = parser.makeExpression("Ceil(x)");
+        ExpressionTreeNode e = parser.makeExpression("ceil(x)");
         assertEquals(new Ceil(new X()), e);
 
         // Test for ceiling of a complex expression: Ceil(x + y)
-        e = parser.makeExpression("Ceil( x + y )");
+        e = parser.makeExpression("ceil( x + y )");
         assertEquals(new Ceil(new Addition(new X(), new Y())), e);
     }
 } 
