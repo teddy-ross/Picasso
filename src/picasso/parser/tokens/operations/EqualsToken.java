b0bbdf257da14c44d9c783ab9b5cfc182084edd1
Nisha <nwalvekar@mail.wlu.edu>
2024-12-04 14:25:57 -0500

    assignment

--- src/picasso/parser/tokens/operations/EqualsToken.java
+++ src/picasso/parser/tokens/operations/EqualsToken.java
@@ -1,11 +1,12 @@
-package picasso.parser.tokens.chars;
+package picasso.parser.tokens.operations;
 
 import picasso.parser.language.CharConstants;
+import picasso.parser.tokens.chars.CharToken;
 
 /**
  * Represents the equals sign in the Picasso programming language
  */
-public class EqualsToken extends CharToken {
+public class EqualsToken extends CharToken implements OperationInterface {
 	public EqualsToken() {
 		super(CharConstants.EQUAL);
 	}
