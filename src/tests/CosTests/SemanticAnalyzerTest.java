eef1b8869dc3f94825a5c6095f5a8526e35c9e0d
Sanjog Basnet <sbasnet@mail.wlu.edu>
2024-12-04 21:47:51 -0500

    fixed cos and sin (from cosine and sine)

--- src/tests/CosTests/SemanticAnalyzerTest.java
+++ src/tests/CosTests/SemanticAnalyzerTest.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package tests.CosineTests;
+package tests.CosTests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,12 +12,12 @@ import org.junit.jupiter.api.Test;
 
 import picasso.parser.SemanticAnalyzer;
 import picasso.parser.language.ExpressionTreeNode;
-import picasso.parser.language.expressions.Cosine;
+import picasso.parser.language.expressions.Cos;
 import picasso.parser.language.expressions.X;
 import picasso.parser.language.expressions.Y;
 import picasso.parser.tokens.IdentifierToken;
 import picasso.parser.tokens.Token;
-import picasso.parser.tokens.functions.CosineToken;
+import picasso.parser.tokens.functions.CosToken;
 
 /**
  * 
@@ -37,23 +37,23 @@ class SemanticAnalyzerTest {
 	}
 
 	@Test
-	void testParseCosine() {
+	void testParseCos() {
 
 		Stack<Token> tokens = new Stack<>();
 		tokens.push(new IdentifierToken("x"));
-		tokens.push(new CosineToken());
+		tokens.push(new CosToken());
 
 		ExpressionTreeNode actual = semAnalyzer.generateExpressionTree(tokens);
 
-		assertEquals(new Cosine(new X()), actual);
+		assertEquals(new Cos(new X()), actual);
 		
 		
 		tokens.push(new IdentifierToken("y"));
-		tokens.push(new CosineToken());
+		tokens.push(new CosToken());
 		
 		actual = semAnalyzer.generateExpressionTree(tokens);
 		
-		assertEquals(new Cosine(new Y()), actual);
+		assertEquals(new Cos(new Y()), actual);
 		
 	}
 
