e5246f83b7a0ad01211cabdac6b5dcc2d08cdf4f
Sanjog Basnet <sbasnet@mail.wlu.edu>
2024-12-06 01:27:21 -0500

    Idk man I tried to fix string and ImgStr

--- src/picasso/parser/ImageclipAnalyzer.java
+++ src/picasso/parser/ImageclipAnalyzer.java
@@ -4,6 +4,7 @@ import java.util.Stack;
 import picasso.parser.language.ExpressionTreeNode;
 import picasso.parser.language.expressions.ImageClip;
 import picasso.parser.language.expressions.ImgNameString;
+import picasso.parser.tokens.StringToken;
 import picasso.parser.tokens.Token;
 
 /**
@@ -13,17 +14,20 @@ import picasso.parser.tokens.Token;
  * @param <ImageNameStrAnalyzer>
  */
 
-public class ImageclipAnalyzer implements SemanticAnalyzerInterface{
+public class ImageClipAnalyzer implements SemanticAnalyzerInterface{
 
 	@Override
 	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
 		tokens.pop(); // Need to remove the imageClip token
 		ExpressionTreeNode parx = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
 		ExpressionTreeNode pary = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
-		
-		ImgNameStrAnalyzer analyzer = new ImgNameStrAnalyzer();
-		ExpressionTreeNode newImage = analyzer.generateExpressionTree(tokens);
-		return new ImageClip((ImgNameString) newImage, parx, pary);
+
+		StringToken token = (StringToken) tokens.pop();
+	
+		String imgName = token.getName();
+
+        // Create and return the ImageClip expression
+        return new ImageClip(imgName, parx, pary);
 		
 	}
 }
