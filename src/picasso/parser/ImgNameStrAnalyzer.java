3a61e9faf14c54cd0a99d675e41e123e5cd27ec8
Sanjog Basnet <sbasnet@mail.wlu.edu>
2024-12-05 23:58:41 -0500

    fixed something idk

--- src/picasso/parser/ImgNameStrAnalyzer.java
+++ src/picasso/parser/ImgNameStrAnalyzer.java
@@ -3,9 +3,7 @@ package picasso.parser;
 import java.util.Stack;
 
 import picasso.parser.language.ExpressionTreeNode;
-import picasso.parser.language.expressions.Equals;
 import picasso.parser.language.expressions.ImgNameString;
-import picasso.parser.tokens.IdentifierToken;
 import picasso.parser.tokens.ImgNameStrToken;
 import picasso.parser.tokens.Token;
 
@@ -20,20 +18,12 @@ public class ImgNameStrAnalyzer implements SemanticAnalyzerInterface {
     @Override
     public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
         
-    	try {
-	        // Pop the token
-	        Token token = tokens.pop();
+        // Pop the token
+        Token token = tokens.pop();
 
-	        // Check if token is an ImgNameStrToken
-	        if (token instanceof ImgNameStrToken) {
-	            ImgNameStrToken imgToken = (ImgNameStrToken) token;
-	            return new ImgNameString(imgToken.getName());
-	        } else {
-	            return null;
-	        }
-	    } catch (ClassCastException e) {
-	        // Handle the exception if necessary (e.g., log the error)
-	        return null;
-	    }
-	}
+        // Cast the token to ImgNameStrToken and create the ImgNameStr node
+        
+        ImgNameStrToken imgToken = (ImgNameStrToken) token;
+        return new ImgNameString(imgToken.getName());
+    }
 }
