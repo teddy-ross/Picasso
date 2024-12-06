8d12a5983b236f0ee4ae566df9a021009e5b7466
Sanjog Basnet <sbasnet@mail.wlu.edu>
2024-12-05 20:23:55 -0500

    ImgNameStr expression, Token (need to do analyzer)

---
+++ src/picasso/parser/ImgNameStrAnalyzer.java
@@ -0,0 +1,20 @@
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.tokens.Token;
+
+public class ImgNameStrAnalyzer implements SemanticAnalyzerInterface {
+
+	public ImgNameStrAnalyzer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/picasso/parser/tokens/ImgNameStrToken.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package picasso.parser.tokens;
+
+/**
+ * Represents an image file (image name string:"img1.jpg")
+ * 
+ * @author Sanjog Basnet
+ */
+public class ImgNameStrToken extends Token {
+
+	private final String imgName;
+
+	/**
+	 * @param description
+	 */
+	public ImgNameStrToken(String value) {
+		super("ImageNameString Token");
+		imgName = value;
+
+	}
+
+	public boolean equals(Object o) {
+		if( o == this ) {
+			return true;
+		}
+		if (!(o instanceof IdentifierToken)) {
+			return false;
+		}
+		ImgNameStrToken rhs = (ImgNameStrToken) o;
+		return imgName.equals(rhs.imgName);
+	}
+
+	/**
+	 * Returns the identifier's name
+	 * @return the identifier's name
+	 */
+	public String getName() {
+		return imgName;
+	}
+
+	public String toString() {
+		return super.toString() + ": " + imgName;
+	}
+
+	@Override
+	public boolean isConstant() {
+		return false;
+	}
+
+	@Override
+	public boolean isFunction() {
+		return false;
+	}
+
+}
