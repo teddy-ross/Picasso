03012a9355a11983f2315d643e3195b9d3b9a0cb
Teddy Ross <tross@mail.wlu.edu>
2024-12-05 22:33:46 -0500

    multiply

--- src/picasso/parser/tokens/CharTokenFactory.java
+++ src/picasso/parser/tokens/CharTokenFactory.java
@@ -39,6 +39,7 @@ public class CharTokenFactory {
 		tokenList[CharConstants.DIVIDE] = new DivideToken();
 		tokenList[CharConstants.MULTIPLY] = new MultiplyToken();
 		tokenList[CharConstants.MOD] = new ModToken();
+		tokenList[CharConstants.QUOTE] = new QuoteToken();
 		// TODO: What other tokens should be in here?
 		// TODO: Need to create the associated Token classes too.
 
