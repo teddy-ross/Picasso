53e775a0f6eacd969d85534bea1781728b0835d1
Sanjog Basnet <sbasnet@mail.wlu.edu>
2024-12-05 22:45:16 -0500

    Implement Subtraction and tests, minor adjustments

--- src/picasso/parser/language/expressions/Subtraction.java
+++ src/picasso/parser/language/expressions/Subtraction.java
@@ -3,19 +3,19 @@ package picasso.parser.language.expressions;
 import picasso.parser.language.ExpressionTreeNode;
 
 /**
- * Represents the Addition operator in the Picasso language.
+ * Represents the Subtraction operator in the Picasso language.
  * @author Sanjog Basnet
  */
 
-public class Addition extends BinaryOperator {
+public class Subtraction extends BinaryOperator {
 
 	/**
-	 * Creates a addition expression tree that takes as parameter the given expression
+	 * Creates a subtraction expression tree that takes as parameter the given expression
 	 * 
 	 * @param left 
 	 * @param right 
 	 */
-	public Addition(ExpressionTreeNode left, ExpressionTreeNode right) {
+	public Subtraction(ExpressionTreeNode left, ExpressionTreeNode right) {
 		super(left, right);
 		
 	}
@@ -23,15 +23,15 @@ public class Addition extends BinaryOperator {
 	public RGBColor evaluate(double x, double y) {
 		RGBColor l = left.evaluate(x, y);
 		RGBColor r = right.evaluate(x, y);
-		double red = l.getRed() + r.getRed();
-		double green = l.getGreen() + r.getGreen();
-		double blue = l.getBlue() + r.getBlue();	
+		double red = l.getRed() - r.getRed();
+		double green = l.getGreen() - r.getGreen();
+		double blue = l.getBlue() - r.getBlue();	
 	return new RGBColor(red,green,blue);
 	}
 	
 	@Override
 	public String getOperator() {
-		return "+";
+		return "-";
 	}
 	
 }
