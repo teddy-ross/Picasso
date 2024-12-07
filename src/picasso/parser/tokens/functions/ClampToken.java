e73267b93f11b86d63fc9ef392657aa586ec0ff9
Nisha <nwalvekar@mail.wlu.edu>
2024-12-07 15:54:18 -0500

    Clamp

---
+++ src/picasso/parser/tokens/functions/ClampToken.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the Clamp Token
+ * @author Nisha Walvekar
+ */
+public class ClampToken extends FunctionToken {
+
+	
+	public ClampToken() {
+		super("Clamp Function Token");
+	}
+
+}
