eb1d798f9a9d700f255a3e12e495d11c9d11d4d9
Sylvia Agatako <sagatako@mail.wlu.edu>
2024-12-09 10:58:43 -0500

    implementation of the perlinBW function.

--- src/picasso/parser/SemanticAnalyzer.java
+++ src/picasso/parser/SemanticAnalyzer.java
@@ -65,6 +65,12 @@ public class SemanticAnalyzer implements SemanticAnalyzerInterface {
 		tokenName = TOKENS_PACKAGE_NAME + "ColorToken";
 		parserName = PARSER_PACKAGE + "ColorAnalyzer";
 		addSemanticAnalyzerMapping(tokenName, parserName);
+		
+		//perlin analyzer
+		
+	    tokenName = TOKENS_PACKAGE_NAME + "functions.PerlinBWToken";
+	    tokenName = PARSER_PACKAGE + "PerlinBWAnalyzer";
+	    addSemanticAnalyzerMapping(tokenName, parserName);
 
 		// TODO: Are there any others that should be added?
 		// Is there a better way to create this mapping?
---
+++ src/picasso/parser/tokens/functions/PerlinBWToken.java
@@ -0,0 +1,16 @@
+
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the perlinBW Token
+ * @author Sylvia Agatako
+ * 
+ */
+public class PerlinBWToken extends FunctionToken {
+
+	public PerlinBWToken() {
+		super("PerlinBW Token");
+	}
+
+}
+
