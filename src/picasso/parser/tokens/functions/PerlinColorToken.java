4aa8888320c5eb59f9c65c756cb794844e1c6581
Teddy Ross <tross@mail.wlu.edu>
2024-12-10 14:14:38 -0500

    perlin color token

---
+++ src/picasso/parser/tokens/functions/PerlinColorToken.java
@@ -0,0 +1,16 @@
+
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the perlinBW Token
+ * @author Edward Ross
+ * 
+ */
+public class PerlinColorToken extends FunctionToken {
+
+	public PerlinColorToken() {
+		super("PerlinColor Token");
+	}
+
+}
+
